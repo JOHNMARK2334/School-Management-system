735cacd1f9049f519f4f88d9d9a9ea30ce3b4395
John Mark <wanjugujohnmark@gmail.com>
2023-08-28 12:56:11 +0300

    commit

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -337,17 +337,32 @@
           </div>
         </div>
         <div class="col-lg-4 col-md-6 mt-4 mb-4">
-          <div id="app">
-            {!! $chart->container() !!}
+          <div class="card z-index-2 ">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+              <div class="bg-gradient-info shadow-info border-radius-lg py-3 pe-1">
+                <div id="chart">
+                  {!! $chart->container() !!}
+                </div>
+                <script src="https://unpkg.com/vue"></script>
+                <script>
+                  var app = new Vue({
+                    el: '#app',
+                  });
+                </script>
+                <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
+                {!! $chart->script() !!}
+              </div>
+            </div>
+            <div class="card-body">
+              <h6 class="mb-0 ">Email Statistics</h6>
+              <p class="text-sm "> (<span class="font-weight-bolder">+25%</span>) increase in monthly logins. </p>
+              <hr class="dark horizontal">
+              <div class="d-flex ">
+                <i class="material-icons text-sm my-auto me-1">schedule</i>
+                <p class="mb-0 text-sm"> updated 4 min ago </p>
+              </div>
+            </div>
           </div>
-          <script src="https://unpkg.com/vue"></script>
-          <script>
-            var app = new Vue({
-              el: '#app',
-            });
-          </script>
-          <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
-          {!! $chart->script() !!}
         </div>
         <div class="col-lg-4 col-md-6 mt-4 mb-4">
           <div class="card z-index-2  ">
