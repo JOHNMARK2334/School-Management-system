4103d0af8beafd7f8e817aaae679306c23adb364
John Mark <wanjugujohnmark@gmail.com>
2023-08-31 09:29:51 +0300

    commit

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,10 +3,101 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
     use HasFactory;
-    protected $fillable=[''];
+    protected $fillable=['message','account_id','category_id','course_id','mpesa_id','role_id','staff_id','student_id','transaction_id'];
+    /**
+     * Account
+     * @return BelongsTo
+     */
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+     /**
+     * Category
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+     /**
+     * Course
+     * @return BelongsTo
+     */
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+     /**
+     * Department
+     * @return BelongsTo
+     */
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+     /**
+     * MPesa
+     * @return BelongsTo
+     */
+    public function mpesa(): BelongsTo
+    {
+        return $this->belongsTo(MPesa::class);
+    }
+     /**
+     * Role
+     * @return BelongsTo
+     */
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+     /**
+     * Staff
+     * @return BelongsTo
+     */
+    public function staff(): BelongsTo
+    {
+        return $this->belongsTo(Staff::class);
+    }
+     /**
+     * Student
+     * @return BelongsTo
+     */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+     /**
+     * Transaction
+     * @return BelongsTo
+     */
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+     /**
+     * Unit
+     * @return BelongsTo
+     */
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
+     /**
+     * User
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
